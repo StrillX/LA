ffdb7822a6cb528c99722f2cae6fe943457f2fb2
Bruno Jardim <61950932+StrillX@users.noreply.github.com>
2020-03-19 22:07:25 +0000

    Update logica.c

--- ProjetoLA/logica.c
+++ ProjetoLA/logica.c
@@ -2,6 +2,7 @@
 #include "logica.h"
 #include "interface.h"
 #include <stdio.h>
+#include <stdlib.h>
 int jogar(ESTADO *e, COORDENADA c) {
 
         if (jogada_valida(e, c)) {
@@ -12,11 +13,37 @@ int jogar(ESTADO *e, COORDENADA c) {
             atualiza_jogador(e);
         } else {
             printf("Jogada Invalida\n");
-            atualiza_ultima(e);
-            muda_pos_ultima(e,c);
+
             interpretador(e);
 
         }
 
     return 1;
+}
+int jogada_valida(ESTADO *e, COORDENADA c){
+    if( (abs(c.coluna-e->ultima_jogada.coluna)<=1 && abs(c.linha-e->ultima_jogada.linha)<=1) ){
+
+        if((consulta_posicao(e,c.coluna+0,c.linha+0))==BRANCA)  {  return 0 ;}
+        else if((consulta_posicao(e,c.coluna+0,c.linha+0))==PRETA)  {  return 0 ;}
+        else return 1;
+    }
+    return 0;
+}
+int fim_jogo(ESTADO *e){
+    if((e->ultima_jogada.coluna==0&&e->ultima_jogada.linha==0)||(e->ultima_jogada.coluna==7&&e->ultima_jogada.linha==7)) return 1;
+    if(!possivel_jogar(e)) return 1;
+    return 0;
+}
+int possivel_jogar(ESTADO *e){
+
+    if(consulta_posicao (e,e->ultima_jogada.coluna+1,e->ultima_jogada.linha+0)==VAZIO) return 1;
+    if(consulta_posicao (e,e->ultima_jogada.coluna+1,e->ultima_jogada.linha+1)==VAZIO) return 1;
+    if(consulta_posicao (e,e->ultima_jogada.coluna+1,e->ultima_jogada.linha-1)==VAZIO) return 1;
+    if(consulta_posicao (e,e->ultima_jogada.coluna-1,e->ultima_jogada.linha+0)==VAZIO) return 1;
+    if(consulta_posicao (e,e->ultima_jogada.coluna-1,e->ultima_jogada.linha+1)==VAZIO) return 1;
+    if(consulta_posicao (e,e->ultima_jogada.coluna-1,e->ultima_jogada.linha-1)==VAZIO) return 1;
+    if(consulta_posicao (e,e->ultima_jogada.coluna+0,e->ultima_jogada.linha+1)==VAZIO) return 1;
+    if(consulta_posicao (e,e->ultima_jogada.coluna+0,e->ultima_jogada.linha-1)==VAZIO) return 1;
+
+    return 0;
 }
