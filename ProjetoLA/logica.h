6cadfc0f1007bb8eaf7c24350e5500b1855fbfb9
Bruno Jardim <61950932+StrillX@users.noreply.github.com>
2020-03-19 22:07:03 +0000

    Update logica.h

--- ProjetoLA/logica.h
+++ ProjetoLA/logica.h
@@ -1,4 +1,8 @@
+
 #ifndef PROJETOLA_LOGICA_H
 #define PROJETOLA_LOGICA_H
 int jogar(ESTADO *e, COORDENADA c);
+int jogada_valida(ESTADO *e, COORDENADA c);
+int fim_jogo(ESTADO *e);
+int possivel_jogar(ESTADO *e);
 #endif //PROJETOLA_LOGICA_H
