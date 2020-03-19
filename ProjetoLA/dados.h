9875aad2a58365f50ff704d1b69dff6893d35939
Bruno Jardim <61950932+StrillX@users.noreply.github.com>
2020-03-19 15:58:28 +0000

    Update dados.h

--- ProjetoLA/dados.h
+++ ProjetoLA/dados.h
@@ -1,12 +1,8 @@
-//
-// Created by bruno on 11/03/20.
-//
-
 #ifndef PROJETOLA_DADOS_H
 #define PROJETOLA_DADOS_H
 
 #define BUF_SIZE 1024
-typedef enum {VAZIO, BRANCA, PRETA,UM,DOIS} CASA;
+    typedef enum {VAZIO, BRANCA, PRETA,UM,DOIS} CASA;
 typedef struct {
     int coluna;
     int linha;
@@ -24,8 +20,18 @@ typedef struct {
     int jogador_atual;
 } ESTADO;
 ESTADO *inicializar_estado();
+
 void atualiza_estado_casa(ESTADO *e,COORDENADA c, CASA);
 void atualiza_ultima(ESTADO *e);
+void atualiza_jogador(ESTADO *e);
+void muda_pos_ultima(ESTADO *e, COORDENADA c);
 
+int consulta_jogador(ESTADO *e);
+
+int jogada_valida(ESTADO *e, COORDENADA c);
+
+int fim_jogo(ESTADO *e);
+int possivel_jogar(ESTADO *e);
 CASA consulta_posicao(ESTADO *e, int x, int y);
+CASA casa_final(ESTADO *e,int x);
 #endif //PROJETOLA_DADOS_H
