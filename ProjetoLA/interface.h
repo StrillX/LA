04c887206267ab9c16df4fbcb5a457a48e465ad3
Bruno Jardim <61950932+StrillX@users.noreply.github.com>
2020-03-19 15:59:47 +0000

    Update interface.h

--- ProjetoLA/interface.h
+++ ProjetoLA/interface.h
@@ -1,9 +1,7 @@
-//
-// Created by bruno on 11/03/20.
-//
 #include "dados.h"
 #ifndef PROJETOLA_INTERFACE_H
 #define PROJETOLA_INTERFACE_H
 void mostrar_tabuleiro(ESTADO *e);
-int interpretador(ESTADO *e);
+int  interpretador    (ESTADO *e);
+void mostra_prompt(ESTADO *e,int ac, int jogada, int jogador);
 #endif //PROJETOLA_INTERFACE_H
