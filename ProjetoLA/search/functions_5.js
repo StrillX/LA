4fa4bb3350de884557573e60cba6cd2e11442288
Bruno Jardim <61950932+StrillX@users.noreply.github.com>
2020-03-21 20:26:05 +0000

    Delete functions_5.js

--- ProjetoLA/search/functions_5.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['jogada_5fvalida',['jogada_valida',['../logica_8h.html#ab1160bacb55fe9977b2a60e615c2e627',1,'logica.c']]],
-  ['jogar',['jogar',['../logica_8h.html#ac313d7e553b1e2b5f266acb9bfff8141',1,'logica.c']]]
-];
