bd51600941bc8d1912b2cc2b2f23266446a1e295
Bruno Jardim <61950932+StrillX@users.noreply.github.com>
2020-03-21 20:15:23 +0000

    Delete all_1.js

--- ProjetoLA/search/all_1.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['casa',['CASA',['../dados_8h.html#aba91601f16d4c485b2d9b8c429f27039',1,'dados.h']]],
-  ['casa_5ffinal',['casa_final',['../dados_8h.html#adc839b5b07de85fcd71f002d76ad378c',1,'dados.c']]],
-  ['consulta_5fjogador',['consulta_jogador',['../dados_8h.html#a2c362b52acc2b5010a20a7aa36818dad',1,'dados.c']]],
-  ['consulta_5fposicao',['consulta_posicao',['../dados_8h.html#a30ed3311c93662ed15ec88bdc86d9855',1,'dados.c']]],
-  ['convertelinha',['convertelinha',['../interface_8h.html#a0096ae2fafe42cc41650df4856ba7fa1',1,'interface.c']]],
-  ['coordenada',['COORDENADA',['../structCOORDENADA.html',1,'']]]
-];
