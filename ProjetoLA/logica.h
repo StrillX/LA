e142ffcc1e98d946bc094f45c3be1efa681c17ab
Bruno Jardim <61950932+StrillX@users.noreply.github.com>
2020-04-23 14:23:40 +0100

    Update logica.h

--- ProjetoLA/logica.h
+++ ProjetoLA/logica.h
@@ -4,6 +4,9 @@ Definição das funções que verificam jogadas e jogam
 */
 #ifndef PROJETOLA_LOGICA_H
 #define PROJETOLA_LOGICA_H
+
+#include "lista.h"
+
 /**
  * \brief Joga numa determinada posição
  * @param e Apontador para o estado
@@ -29,4 +32,6 @@ int fim_jogo(ESTADO *e);
  * @return 1 ou 0, dependendo de ser verdadeiro ou nao
  */
 int possivel_jogar(ESTADO *e);
+LISTA listagem_de_jogadas(ESTADO *e,LISTA jogadas);
+double distancia (COORDENADA c, COORDENADA destino);
 #endif //PROJETOLA_LOGICA_H
