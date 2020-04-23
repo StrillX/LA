3cf03bc5eeae3e9c4b6474a5e8fee4db7072913c
Bruno Jardim <61950932+StrillX@users.noreply.github.com>
2020-04-23 14:21:41 +0100

    Update main.c

--- ProjetoLA/main.c
+++ ProjetoLA/main.c
@@ -2,7 +2,11 @@
 #include "dados.h"
 #include "logica.h"
 #include "interface.h"
+#include <time.h>
+#include <stdlib.h>
+
 int main() {
+    srand(time(NULL));
     ESTADO *e = inicializar_estado();
 
     mostrar_tabuleiro(e,stdout);
